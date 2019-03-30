fc9b045ec23c82816c356263bc7fe410d316e5ac
iori2a <1491247540@qq.com>
2019-03-30 16:21:49 +0800

    AfxWinMain 屏蔽内容 可编译运行
    AfxWinMain 屏蔽相关细节内容 只留主框架

--- MFC/MFC/winmain.cpp
+++ MFC/MFC/winmain.cpp
@@ -19,46 +19,46 @@
 int AFXAPI AfxWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	_In_ LPTSTR lpCmdLine, int nCmdShow)
 {
-	ASSERT(hPrevInstance == NULL);
+	//ASSERT(hPrevInstance == NULL);
 
 	int nReturnCode = -1;
-	CWinThread* pThread = AfxGetThread();
-	CWinApp* pApp = AfxGetApp();
-
-	// AFX internal initialization
-	if (!AfxWinInit(hInstance, hPrevInstance, lpCmdLine, nCmdShow))
-		goto InitFailure;
-
-	// App global initializations (rare)
-	if (pApp != NULL && !pApp->InitApplication())
-		goto InitFailure;
-
-	// Perform specific initializations
-	if (!pThread->InitInstance())
-	{
-		if (pThread->m_pMainWnd != NULL)
-		{
-			TRACE(traceAppMsg, 0, "Warning: Destroying non-NULL m_pMainWnd\n");
-			pThread->m_pMainWnd->DestroyWindow();
-		}
-		nReturnCode = pThread->ExitInstance();
-		goto InitFailure;
-	}
-	nReturnCode = pThread->Run();
-
-InitFailure:
-#ifdef _DEBUG
-	// Check for missing AfxLockTempMap calls
-	if (AfxGetModuleThreadState()->m_nTempMapLock != 0)
-	{
-		TRACE(traceAppMsg, 0, "Warning: Temp map lock count non-zero (%ld).\n",
-			AfxGetModuleThreadState()->m_nTempMapLock);
-	}
-	AfxLockTempMaps();
-	AfxUnlockTempMaps(-1);
-#endif
-
-	AfxWinTerm();
+	//CWinThread* pThread = AfxGetThread();
+//	CWinApp* pApp = AfxGetApp();
+//
+//	// AFX internal initialization
+//	if (!AfxWinInit(hInstance, hPrevInstance, lpCmdLine, nCmdShow))
+//		goto InitFailure;
+//
+//	// App global initializations (rare)
+//	if (pApp != NULL && !pApp->InitApplication())
+//		goto InitFailure;
+//
+//	// Perform specific initializations
+//	if (!pThread->InitInstance())
+//	{
+//		if (pThread->m_pMainWnd != NULL)
+//		{
+//			TRACE(traceAppMsg, 0, "Warning: Destroying non-NULL m_pMainWnd\n");
+//			pThread->m_pMainWnd->DestroyWindow();
+//		}
+//		nReturnCode = pThread->ExitInstance();
+//		goto InitFailure;
+//	}
+//	nReturnCode = pThread->Run();
+//
+//InitFailure:
+//#ifdef _DEBUG
+//	// Check for missing AfxLockTempMap calls
+//	if (AfxGetModuleThreadState()->m_nTempMapLock != 0)
+//	{
+//		TRACE(traceAppMsg, 0, "Warning: Temp map lock count non-zero (%ld).\n",
+//			AfxGetModuleThreadState()->m_nTempMapLock);
+//	}
+//	AfxLockTempMaps();
+//	AfxUnlockTempMaps(-1);
+//#endif
+//
+//	AfxWinTerm();
 	return nReturnCode;
 }
 
